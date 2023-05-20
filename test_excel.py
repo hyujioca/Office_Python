493264891e85cc2f4e25dd97bdcd98f48dbc2e03
cre <2104240035oh@edu.oca.ac.jp>
2023-05-20 16:31:27 +0900

    aaa

--- test_excel.py
+++ test_excel.py
@@ -13,9 +13,13 @@ ws.append([1, 2, 3])
 # Python types will automatically be converted
 import datetime
 ws['A2'] = datetime.datetime.now()
-for x in range(1,101):
-    for y in range(1,101):
-        ws.cell(row=x, column=y)
+
+ws_new = wb.create_sheet("追加シート")
+value_data = 0
+for x in range(1,11):
+    for y in range(1,11):
+        value_data = 10 * (x - 1) + y
+        ws_new.cell(row=x, column=y).value = value_data
 # Save the file
 wb.save("sample.xlsx")
 
